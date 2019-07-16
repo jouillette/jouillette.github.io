27e5c63d6396f65b83a62845d89ee14e6ce95a74
a2rocklobster <joeouillette@gmail.com>
2019-07-16 17:18:56 -0400

    Tuesday - added content

--- js/site.js
+++ js/site.js
@@ -1,38 +1,49 @@
 jQuery(document).ready(function($){
-	$('.main-nav li').on('click', function(){
+
+	// section nav
+	$('.section-nav').on('click', function(){
 		const section = $(this).data('id');
 		$("html, body").animate({ scrollTop: $('.'+section).offset().top - 50}, 500, "easeInOutCubic");
 	});
-
-	// $('#main > section').each(function(){
-	// 	$('.wrapper', this).css('opacity', 0);
-	// });
 
 	$(window).on('scroll', function(){
 		let scroll = $(window).scrollTop();
 		let window_height = window.innerHeight;
 		let view = scroll + window_height;
 
+		// section visibility
 		$('#main > section .wrapper').each(function(){
 			if($(this).offset().top + 100 < view){
 				$(this).removeClass('hidden');
 			}else{
 				$(this).addClass('hidden');
 			}
+		});		
+
+		// section visibility
+		let active = 'cover';
+		$($('#main > section .wrapper').get().reverse()).each(function(){
+			if($(this).offset().top - 150 <= scroll){
+				active = $(this).parent('section').attr('class');
+				return false;
+			}
 		});
+
+		// do the nav color
+		$('.section-nav').removeClass('active');
+		$('.section-nav[data-id="'+active+'"]').addClass('active');
+
 	}).scroll();
 
-
-	var i = 0;
-		var txt = $('.type-this').text();
-		$('.type-this').html('');
-		typeWriter();
+	// home typing
+	let i = 0;
+	let txt = $('.type-this').text();
+	let speed = 50; 
+	$('.type-this').html('');
+	typeWriter();
 	
-	function typeWriter() {	
-
-		
-		var speed = 50; 
-		if (i < txt.length) {
+	function typeWriter(){	
+		if(i < txt.length){
 			$('.type-this').append(txt.charAt(i));
 			i++;
 			setTimeout(typeWriter, speed);
